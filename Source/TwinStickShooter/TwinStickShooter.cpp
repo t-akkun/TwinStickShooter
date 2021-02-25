83dc6e2d7b7171f36bb90e3e39aff239856424f6
t-akkun <76903735+t-akkun@users.noreply.github.com>
2021-02-26 00:30:00 +0900

    Initial commit

---
+++ Source/TwinStickShooter/TwinStickShooter.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TwinStickShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TwinStickShooter, "TwinStickShooter" );
---
+++ Source/TwinStickShooter/TwinStickShooterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TwinStickShooterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWINSTICKSHOOTER_API ATwinStickShooterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
